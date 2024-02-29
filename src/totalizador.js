d2139965b7445e0e740569be8b03007a54d09c8b
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-02-29 09:42:07 -0400

    funcionalidad: Mostrar mensaje de error si es que el precio o la cantidad ingresados son menores o iguales a 0 added

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,4 +1,7 @@
 function totalizar(precio, cantidad) {
+  if(precio <= 0 || cantidad <=0){
+    return "Error, la cantidad y el precio deben ser mayores a 0";
+  }
   return precio * cantidad;
 }
 
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -4,10 +4,16 @@ describe("Obtener Precio Neto", () => {
   it("deberia mostrar el precio neto para un item", () => {
     expect(totalizar(20, 1)).toEqual(20);
   });
+
   it("deberia mostrar el precio neto para un item", () => {
     expect(totalizar(30, 1)).toEqual(30);
   });
+
   it("deberia mostrar el precio neto para varios items", () => {
     expect(totalizar(30, 2)).toEqual(60);
+  });
+
+  it("deberia mostrar un mensaje de error si el precio o la cantidad son menores o iguales a 0", () => {
+    expect(totalizar(0, -2)).toEqual("Error, la cantidad y el precio deben ser mayores a 0");
   });
 });
