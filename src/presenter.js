89bb0d7a97a6aeb5fdaf22cd4dba82e52a7b33a2
Diegodenac <diegocanedo917@gmail.com>
2024-03-05 00:22:22 -0400

    Refactoring para interfaz arreglos en precio neto y mostrar impuesto segun el estado

--- src/presenter.js
+++ src/presenter.js
@@ -1,12 +1,14 @@
-import totalizar from "./totalizador";
+import {totalizar, get_descuento} from "./totalizador";
 
 const precioUnidad = document.querySelector("#precio");
 const cantidad = document.querySelector("#cantidad");
 const form = document.querySelector("#totalizador-form");
 const estado = document.querySelector('#estado')
 const divPrecioNeto = document.querySelector("#precio-neto");
+const divImpuesto = document.querySelector("#impuesto");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  divPrecioNeto.innerHTML = "<p> El precio neto es: " + parseInt(totalizar(precioUnidad.value, cantidad.value)) + "</p>";
+  divPrecioNeto.innerHTML = "<p> Precio neto(" + cantidad.value + "*$" + precioUnidad.value + "): " + parseInt(totalizar(precioUnidad.value, cantidad.value)) + "</p>";
+  divImpuesto.innerHTML = "<p> Impuesto para " + estado.value + "(%" + parseFloat(get_descuento(estado.value)) + "): ";
 });
