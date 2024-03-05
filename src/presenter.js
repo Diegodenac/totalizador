df5edc8868be3e40e1369d84a5d6bac73eee1063
Diegodenac <diegocanedo917@gmail.com>
2024-03-05 00:51:11 -0400

    Interfaz mostrar Precio Total con impuesto

--- src/presenter.js
+++ src/presenter.js
@@ -6,11 +6,14 @@ const form = document.querySelector("#totalizador-form");
 const estado = document.querySelector('#estado')
 const divPrecioNeto = document.querySelector("#precio-neto");
 const divImpuesto = document.querySelector("#impuesto");
+const divPrecioTotal = document.querySelector("#precio-total");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   precioNeto = totalizar(precioUnidad.value, cantidad.value);
   porcentajeImpuesto = get_impuesto(estado.value);
+  impuesto = calcular_impuesto(precioNeto, porcentajeImpuesto);
   divPrecioNeto.innerHTML = "<p> Precio neto(" + cantidad.value + "*$" + precioUnidad.value + "): " + parseInt(precioNeto) + "</p>";
-  divImpuesto.innerHTML = "<p> Impuesto para " + estado.value + "(%" + parseFloat(porcentajeImpuesto) + "): " + parseFloat(calcular_impuesto(precioNeto, porcentajeImpuesto)) + "</p>";
+  divImpuesto.innerHTML = "<p> Impuesto para " + estado.value + "(%" + parseFloat(porcentajeImpuesto) + "): " + parseFloat(impuesto) + "</p>";
+  divPrecioTotal.innerHTML = "<p> Precio Total (impuesto): " + "</p>"; //calcular_pt(precioNeto, impuesto)
 });
