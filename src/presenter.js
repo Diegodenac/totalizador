355106df538b21d5f955f1b6101e8913e82e24e6
Diegodenac <diegocanedo917@gmail.com>
2024-03-04 23:56:26 -0400

    Interfaz para seleccionar estado de CA

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import totalizar from "./totalizador";
 const precioUnidad = document.querySelector("#precio");
 const cantidad = document.querySelector("#cantidad");
 const form = document.querySelector("#totalizador-form");
+const estado = document.querySelector('#estado')
 const divPrecioNeto = document.querySelector("#precio-neto");
 
 form.addEventListener("submit", (event) => {
