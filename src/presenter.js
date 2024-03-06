3414000d57faf46357403157f07b340a3424aa55
Diegodenac <diegocanedo917@gmail.com>
2024-03-05 22:20:47 -0400

    Interfaz para mostrar porcentaje de descuento y descuento added

--- src/presenter.js
+++ src/presenter.js
@@ -5,15 +5,18 @@ const cantidad = document.querySelector("#cantidad");
 const form = document.querySelector("#totalizador-form");
 const estado = document.querySelector('#estado')
 const divPrecioNeto = document.querySelector("#precio-neto");
+const divDescuento = document.querySelector("#descuento");
 const divImpuesto = document.querySelector("#impuesto");
 const divPrecioTotal = document.querySelector("#precio-total");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let precioNeto = totalizar(precioUnidad.value, cantidad.value);
+  //let porcentajeDescuento = get_descuento(cantidad.value);
   let porcentajeImpuesto = get_impuesto(estado.value);
   let impuesto = calcular_impuesto(precioNeto, porcentajeImpuesto);
   divPrecioNeto.innerHTML = "<p> Precio neto(" + cantidad.value + "*$" + precioUnidad.value + "): " + parseInt(precioNeto) + "</p>";
+  divDescuento.innerHTML = "<p> Descuento ( %):" + "</p>"; //calcular_descuento(precioNeto, porcentajeDescuento)
   divImpuesto.innerHTML = "<p> Impuesto para " + estado.value + "(%" + parseFloat(porcentajeImpuesto) + "): " + parseFloat(impuesto) + "</p>";
   divPrecioTotal.innerHTML = "<p> Precio Total (impuesto): " + calcular_pt(precioNeto, impuesto) + "</p>";
 });
