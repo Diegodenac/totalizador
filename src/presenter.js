cd40b9822bdcb4a8690bbda9019969b55cd91d3b
Diegodenac <diegocanedo917@gmail.com>
2024-03-05 22:58:15 -0400

    Interfaz actualizada para mostrar descuento y calcular precio total

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import {totalizar, get_impuesto, calcular_impuesto, calcular_pt} from "./totalizador";
+import {totalizar, get_impuesto, get_descuento, calcular_impuesto, calcular_descuento, calcular_pt} from "./totalizador";
 
 const precioUnidad = document.querySelector("#precio");
 const cantidad = document.querySelector("#cantidad");
@@ -12,11 +12,13 @@ const divPrecioTotal = document.querySelector("#precio-total");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let precioNeto = totalizar(precioUnidad.value, cantidad.value);
-  //let porcentajeDescuento = get_descuento(cantidad.value);
+  let porcentajeDescuento = get_descuento(cantidad.value);
+  let montoDescuento = calcular_descuento(precioNeto, porcentajeDescuento);
+  let precioDescuento = precioNeto-montoDescuento;
   let porcentajeImpuesto = get_impuesto(estado.value);
-  let impuesto = calcular_impuesto(precioNeto, porcentajeImpuesto);
-  divPrecioNeto.innerHTML = "<p> Precio neto(" + cantidad.value + "*$" + precioUnidad.value + "): " + parseInt(precioNeto) + "</p>";
-  divDescuento.innerHTML = "<p> Descuento ( %):" + "</p>"; //calcular_descuento(precioNeto, porcentajeDescuento)
-  divImpuesto.innerHTML = "<p> Impuesto para " + estado.value + "(%" + parseFloat(porcentajeImpuesto) + "): " + parseFloat(impuesto) + "</p>";
-  divPrecioTotal.innerHTML = "<p> Precio Total (impuesto): " + calcular_pt(precioNeto, impuesto) + "</p>";
+  let montoImpuesto = calcular_impuesto(precioDescuento, porcentajeImpuesto);
+  divPrecioNeto.innerHTML = "<p> Precio neto(" + cantidad.value + "*$" + precioUnidad.value + "): " + parseFloat(precioNeto) + "</p>";
+  divDescuento.innerHTML = "<p> Descuento (" + porcentajeDescuento +"%): " + montoDescuento + "</p>";
+  divImpuesto.innerHTML = "<p> Impuesto para " + estado.value + "(%" + parseFloat(porcentajeImpuesto) + "): " + parseFloat(montoImpuesto) + "</p>";
+  divPrecioTotal.innerHTML = "<p> Precio Total (impuesto): " + calcular_pt(precioNeto, montoImpuesto, montoDescuento) + "</p>";
 });
